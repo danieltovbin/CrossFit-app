7da03be5c6285c20098bcb36367d32ef254c2b55
danieltovbin <111869103+danieltovbin@users.noreply.github.com>
2024-06-30 23:22:42 +0300

    Creating 404 page

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
 import HomePage from '../pages/HomePage/HomePage';
-import ShopPage from '../pages/ShopPage/ShopPage';
+import PageNotFound from '../pages/404Page/PageNotFound';
 
 export const router = createBrowserRouter([
     { path: "/", element: < HomePage />},
-    { path: "/shop", element: < ShopPage/>},
+    { path: "/shop", element: < Header/>},
+    { path: "*", element: < PageNotFound/>},
 
 ])
