a6d918b92c50bc8113e529a4dd3045a1a3081e0f
danieltovbin <111869103+danieltovbin@users.noreply.github.com>
2024-06-30 23:58:36 +0300

    changing the name of the label

--- client/src/components/ContactUs/ValidatedForm/ValidatedForm.jsx
+++ client/src/components/ContactUs/ValidatedForm/ValidatedForm.jsx
@@ -26,7 +26,7 @@ const ValidatedForm = () => {
     >
       <Row className="mb-3">
         <Form.Group as={Col} md="4" controlId="validationCustom01">
-          <Form.Label>Full name</Form.Label>
+          <Form.Label>First name</Form.Label>
           <Form.Control required type="text" placeholder="First name" />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
 import HomePage from '../pages/HomePage/HomePage';
 import PageNotFound from '../pages/404Page/PageNotFound';
+import ShopPage from '../pages/ShopPage/ShopPage';
 
 export const router = createBrowserRouter([
     { path: "/", element: < HomePage />},
-    { path: "/shop", element: < Header/>},
+    { path: "/shop", element: < ShopPage/>},
     { path: "*", element: < PageNotFound/>},
 
 ])
