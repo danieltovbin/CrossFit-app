b71a4cc99b54766b7744b06abefb33fee08bbfab
danieltovbin <111869103+danieltovbin@users.noreply.github.com>
2024-07-11 18:00:19 +0300

    Adding spinner while loading the products in shop page

--- client/src/pages/ShopPage/ShopPage.jsx
+++ client/src/pages/ShopPage/ShopPage.jsx
@@ -3,30 +3,36 @@ import React, { useContext, useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Header from "../../components/Header/Header";
+import Spinner from "react-bootstrap/Spinner";
 import "./style.scss";
 import { CartContext } from "../../context/cartContext";
 
 const ShopPage = () => {
   const [products, setProducts] = useState([]);
   const { addToCart } = useContext(CartContext);
+  const [loading, setLoading] = useState(true);
   const [added, setAdded] = useState({});
 
   const fetchProducts = async () => {
     try {
-      const response = await axios.get(`https://crossfit-web-server.onrender.com/API/products/get`);
+      const response = await axios.get(
+        `https://crossfit-web-server.onrender.com/API/products/get`
+      );
       setProducts(response.data);
     } catch (error) {
       console.error("Error fetching products", error);
+    } finally {
+      setLoading(false);
     }
   };
 
   const handleCartBtn = (product) => {
     addToCart(product);
-    setAdded((prev) => ({...prev, [product._id]: true}));
+    setAdded((prev) => ({ ...prev, [product._id]: true }));
     setTimeout(() => {
-      setAdded((prev) => ({...prev, [product._id]: false}));
+      setAdded((prev) => ({ ...prev, [product._id]: false }));
     }, 3000);
-  }
+  };
 
   useEffect(() => {
     fetchProducts();
@@ -37,7 +43,11 @@ const ShopPage = () => {
       <Header />
       <h1>Accessories</h1>
       <div className="products-container">
-        {products.length === 0 ? (
+        {loading ? (
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        ) : products.length === 0 ? (
           <p>No products found</p>
         ) : (
           products.map((product) => (
@@ -52,9 +62,17 @@ const ShopPage = () => {
               <Card.Body>
                 <Card.Title> {product.name}</Card.Title>
                 <Card.Text>{`$${product.price}`}</Card.Text>
-                <Button onClick={() => handleCartBtn(product)} variant={added[product._id] ? "light" : "outline-primary"} disabled={added[product._id]}>
-                  <i className={added[product._id]? "bi bi-check-lg" : "bi bi-cart"}></i>
-                  {added[product._id]? "Added": "Add To Cart"}
+                <Button
+                  onClick={() => handleCartBtn(product)}
+                  variant={added[product._id] ? "light" : "outline-primary"}
+                  disabled={added[product._id]}
+                >
+                  <i
+                    className={
+                      added[product._id] ? "bi bi-check-lg" : "bi bi-cart"
+                    }
+                  ></i>
+                  {added[product._id] ? "Added" : "Add To Cart"}
                 </Button>
               </Card.Body>
             </Card>
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/API': {
-  //       target: 'https://crossfit-web-server.onrender.com',},
-  //       changeOrigin: true, 
-  //   },
-  // },
 })
