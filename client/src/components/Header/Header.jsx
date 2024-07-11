f20d6664b2e28309218f70f90388a5ee5c6aeb24
danieltovbin <111869103+danieltovbin@users.noreply.github.com>
2024-07-11 12:00:22 +0300

    changing href to as Link

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 import "./style.scss";
 import Cart from "../Cart/Cart";
 
@@ -9,13 +10,13 @@ const Header = () => {
   return (
     <Navbar className="custom-header" sticky="top" expand="lg" bg="dark" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="/">CrossFit</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">CrossFit</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#getting-started">GETTING STARTED</Nav.Link>
-            <Nav.Link href="/shop">SHOP</Nav.Link>
-            <Nav.Link href="#contact-us">CONTACT US</Nav.Link>
+            <Nav.Link as={Link} to="#getting-started">GETTING STARTED</Nav.Link>
+            <Nav.Link as={Link} to="/shop">SHOP</Nav.Link>
+            <Nav.Link as={Link} to="#contact-us">CONTACT US</Nav.Link>
           </Nav>
           <Nav className="social-media">
             <Nav.Link
