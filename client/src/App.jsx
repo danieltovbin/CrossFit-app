61577fc5a644ca34a6db54ca03dfab74a0a8d78e
danieltovbin <111869103+danieltovbin@users.noreply.github.com>
2024-07-11 11:50:33 +0300

    disableReactDevTools

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/routes'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+disableReactDevTools();
 
 function App() {
 
