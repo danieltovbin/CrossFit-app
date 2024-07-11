0d3c947b0671ef3fe64939a7fb2e42a8c3d999fe
danieltovbin <111869103+danieltovbin@users.noreply.github.com>
2024-07-11 11:44:26 +0300

    removing server from proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/API': {
-        target: 'https://crossfit-web-server.onrender.com',},
-        changeOrigin: true, 
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/API': {
+  //       target: 'https://crossfit-web-server.onrender.com',},
+  //       changeOrigin: true, 
+  //   },
+  // },
 })
